8ea2ff5ffe5799ac7af28b0a11172e9a5ffa7f7d
omazarak <readywebsolutions@gmail.com>
2022-03-25 11:57:35 +0200

    ListController

---
+++ src/Controller/ListController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/ListController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ListController
+{
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>List number: '.$number.'</body></html>'
+        );
+    }
+}
