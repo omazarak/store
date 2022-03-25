15f6ded0942b10572015f9fa4fc7c1069730de1b
omazarak <readywebsolutions@gmail.com>
2022-03-25 15:31:51 +0200

    bootstrap

---
+++ config/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+// bootstrap.php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$proxyDir = null;
+$cache = null;
+$useSimpleAnnotationReader = false;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/Entity"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+// or if you prefer yaml or XML
+// $config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+// $config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    // url: '%env(resolve:DATABASE_URL)%'
+    'path' => __DIR__ . '/test.db',
+);
+
+// obtaining the entity manager
+try {
+    $entityManager = EntityManager::create($conn, $config);
+} catch (\Exception $e) {
+    echo('*** failed to connect');
+}
