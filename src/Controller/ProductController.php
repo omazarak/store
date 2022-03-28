078033dd6bb335a4b9710c444737c7de5ae4ecb6
omazarak <readywebsolutions@gmail.com>
2022-03-28 13:32:11 +0300

    View List and Product by id

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,20 @@
 // src/Controller/ProductController.php
 namespace App\Controller;
 
+use App\Entity\Product;
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-    public function by_number(): Response
+    #[Route('/view/{id}')]
+    public function by_id(Product $product): Response
     {
-        $number = random_int(0, 100);
-
-        return $this->render('description.html.twig', [
-            'number' => $number,
+        return $this->render('product.html.twig', [
+            'id' => $product->getId(),
+            'name' => htmlspecialchars($product->getName(), \ENT_COMPAT | \ENT_HTML5),
         ]);
     }
 }
